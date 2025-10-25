7f18bb85d383e499f5def928b67876b9e875520b
Jiaheng <jiaheng@Jiahengs-MacBook.local>
2025-10-24 20:26:12 -0400

    update results

--- src/eval.py
+++ src/eval.py
@@ -1,18 +1,1 @@
-def precision_score(tp, fp):
-    """Compute precision score.
-
-    Parameters
-    ----------
-        tp: int
-            True positives
-        fp: int
-            False positives
-
-    Returns
-    -------
-        precision: float
-            Precision score
-    """
-    if tp + fp == 0:
-        return 0.0
-    return tp / (tp + fp)
+from sklearn.metrics import precision_score, recall_score, roc_auc_score
